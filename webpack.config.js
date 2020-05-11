51278085a68237ed47fdaad3b8bddf1aedd5789b
ComradAlex <55763012+ComradAlex@users.noreply.github.com>
2020-05-12 02:04:28 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('C:\Users\Alex\Documents\Lab4', 'dist')
+  }
+};
