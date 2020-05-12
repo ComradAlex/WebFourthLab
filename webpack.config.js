22b4e2ac3e8212c40234b8881cc60df9e80ef781
ComradAlex <55763012+ComradAlex@users.noreply.github.com>
2020-05-12 13:33:50 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve('C:\Users\Alex\Documents\Lab4', 'dist')
+    path: path.resolve('C:/Users/Alex/Documents/Lab4', 'dist')
   }
 };
